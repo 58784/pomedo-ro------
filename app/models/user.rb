205b2c3c2d0c023e1f332a91fa34bd01e91c0b18
58784 <k56k.k-0504@docomo.ne.jp>
2022-05-16 23:49:51 +0900

    ログイン/ログアウト機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :email, uniqueness: true
+
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
