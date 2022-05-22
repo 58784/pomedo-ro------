3f5bad2acab168852f5b702fd779b6a49cd5fbde
58784 <k56k.k-0504@docomo.ne.jp>
2022-05-22 15:28:27 +0900

    Fix: タイマーモデルの修正

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -4,6 +4,8 @@ class Timer < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :work_time, presence: true
   validates :rest_time, presence: true
+  validates :start_time, presence: true
+  validates :finish_time, presence: true
 
   enum switch: { off: 0, on: 1 }
   enum character: { pome: 0 }
